cfa899a3bc3ba125c0277e13399717ab7422c9db
yamagame <yamagame@mac.com>
2023-09-02 07:41:54 +0900

    mainもどし

--- main.go
+++ main.go
@@ -1,61 +1,115 @@
 package main
 
 import (
+	"errors"
+	"fmt"
+	"sample/go-gorm-example/conv"
 	"sample/go-gorm-example/infra"
 	"sample/go-gorm-example/model"
+	"sample/go-gorm-example/repository/company"
+	"sample/go-gorm-example/repository/user"
 
-	"gorm.io/gen"
+	"gorm.io/gorm"
 )
 
 func main() {
 	db := infra.DB()
 
 	db.AutoMigrate(&model.User{})
-	db.AutoMigrate(&model.Company{})
+	// UserをAutoMigrateすれば自動的にCompanyができるため不要
+	// db.AutoMigrate(&model.Company{})
 
-	g := gen.NewGenerator(gen.Config{
-		// 生成ディレクトリ、パッケージ名になる
-		OutPath: "./infra/dao",
-		// モード
-		Mode:              gen.WithoutContext | gen.WithQueryInterface,
-		FieldWithIndexTag: true,
-		FieldNullable:     true,
-		WithUnitTest:      true,
-		FieldWithTypeTag:  true,
-	})
+	db.Transaction(func(tx *gorm.DB) error {
+		companyRepo := company.NewRepository(tx)
+		userRepo := user.NewRepository(tx)
 
-	// gorm.DBを指定する
-	g.UseDB(db)
+		var err error
 
-	// 全てのテーブルを取得
-	tableList, err := db.Migrator().GetTables()
-	if err != nil {
-		panic(err)
-	}
-
-	remove := func(l []string, item string) []string {
-		for i, other := range l {
-			if other == item {
-				return append(l[:i], l[i+1:]...)
-			}
+		// 会社Aの作成
+		companyA, err := companyRepo.Create(&model.Company{
+			Code: conv.StrP("001"),
+			Name: conv.StrP("会社A"),
+		})
+		if err != nil {
+			panic(err)
 		}
-		return l
-	}
+		fmt.Println(companyA)
 
-	// 各テーブル毎にモデルを作成
-	tables := make([]interface{}, len(tableList))
-	tables = append(tables, g.GenerateModel("users", gen.FieldType("role", "*Role")))
-	remove(tableList, "users")
+		// 会社Bの作成
+		companyB, err := companyRepo.Create(&model.Company{
+			Code: conv.StrP("002"),
+			Name: conv.StrP("会社B"),
+		})
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(companyB)
 
-	// 残りのテーブルのモデルを作成
-	for _, tableName := range tableList {
-		tables = append(tables, g.GenerateModel(tableName))
-	}
+		names := []string{
+			"社員A",
+			"社員B",
+			"社員C",
+		}
 
-	// DAOを生成
-	g.ApplyBasic(
-		tables...,
-	)
-	// コードを生成
-	g.Execute()
+		// 会社Aの社員の作成
+		for _, name := range names {
+			user, err := userRepo.Create(&model.User{
+				Name:    conv.StrP(name + "-A"),
+				Company: companyA,
+			})
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println(user)
+		}
+
+		// 会社Bの社員の作成
+		for _, name := range names {
+			user, err := userRepo.Create(&model.User{
+				Name:    conv.StrP(name + "-B"),
+				Company: companyB,
+			})
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println(user)
+		}
+
+		// 社員A-Aを検索
+		userA1, err := userRepo.Find(&model.User{Name: conv.StrP("社員A-A")})
+		if err != nil {
+			panic(err)
+		}
+
+		// 社員A-Aの名前と年齢変更
+		userA1.Name = conv.StrP("社員X-A")
+		userA1.Age = conv.UintP(34)
+		userA2, err := userRepo.Update(userA1)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(userA2)
+
+		// 会社A社員一覧
+		usersA, err := userRepo.List(&model.User{CompanyID: &companyA.ID})
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(usersA)
+
+		// 会社Bの社員Bを削除
+		err = userRepo.Delete(&model.User{Name: conv.StrP("社員B-B"), CompanyID: &companyB.ID})
+		if err != nil {
+			panic(err)
+		}
+
+		// 会社B社員一覧
+		usersB, err := userRepo.List(&model.User{CompanyID: &companyB.ID})
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(usersB)
+
+		return errors.New("rollback")
+	})
 }
