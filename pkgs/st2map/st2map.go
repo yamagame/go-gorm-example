30d787dc28dc6f454263ec6cad1a3d855a7a8099
yamagame <yamagame@mac.com>
2023-09-03 08:02:19 +0900

    リファクタ

--- pkgs/st2map/st2map.go
+++ pkgs/st2map/st2map.go
@@ -17,6 +17,28 @@ func StructToMap[T any](v T, tag string) map[string]Field {
 	return structToMap(rv, tag)
 }
 
+func fieldValue(fv reflect.Value, tag string) interface{} {
+	switch fv.Kind() {
+	case reflect.Bool:
+		return fv.Bool()
+	case reflect.Int, reflect.Int64, reflect.Int32, reflect.Int16, reflect.Int8:
+		return fv.Int()
+	case reflect.Uint, reflect.Uint64, reflect.Uint32, reflect.Uint16, reflect.Uint8:
+		return fv.Uint()
+	case reflect.Float32, reflect.Float64:
+		return fv.Float()
+	case reflect.String:
+		return fv.String()
+	case reflect.Struct:
+		return structToMap(fv, tag)
+	case reflect.Pointer:
+		v := fv.Elem() // return reflect.Value
+		return fieldValue(v, tag)
+	default:
+		panic(fmt.Errorf("no support type %v", fv.Kind()))
+	}
+}
+
 func structToMap(rv reflect.Value, tag string) map[string]Field {
 	rt := rv.Type()
 	h := map[string]Field{}
@@ -29,38 +51,7 @@ func structToMap(rv reflect.Value, tag string) map[string]Field {
 		fv := rv.FieldByName(f.Name) // return reflect.Value
 		field.Tag = f.Tag.Get(tag)
 		field.Type = fv.Type().String()
-		switch fv.Kind() {
-		case reflect.Bool:
-			field.Value = fv.Bool()
-		case reflect.Int, reflect.Int64, reflect.Int32, reflect.Int16, reflect.Int8:
-			field.Value = fv.Int()
-		case reflect.Uint, reflect.Uint64, reflect.Uint32, reflect.Uint16, reflect.Uint8:
-			field.Value = fv.Uint()
-		case reflect.Float32, reflect.Float64:
-			field.Value = fv.Float()
-		case reflect.String:
-			field.Value = fv.String()
-		case reflect.Struct:
-			field.Value = structToMap(fv, tag)
-		case reflect.Pointer:
-			v := fv.Elem() // return reflect.Value
-			switch v.Kind() {
-			case reflect.Bool:
-				field.Value = v.Bool()
-			case reflect.Int, reflect.Int64, reflect.Int32, reflect.Int16, reflect.Int8:
-				field.Value = v.Int()
-			case reflect.Uint, reflect.Uint64, reflect.Uint32, reflect.Uint16, reflect.Uint8:
-				field.Value = v.Uint()
-			case reflect.Float32, reflect.Float64:
-				field.Value = v.Float()
-			case reflect.String:
-				field.Value = v.String()
-			default:
-				break
-			}
-		default:
-			fmt.Println(fv.String())
-		}
+		field.Value = fieldValue(fv, tag)
 		h[f.Name] = field
 	}
 	return h
