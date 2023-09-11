fb6b8e6f898be0643dd5c4af62c9c68ef0bcb9db
yamagame <yamagame@mac.com>
2023-09-11 22:51:53 +0900

    コード分離

--- pkgs/csvconv/gateway.go
+++ pkgs/csvconv/gateway.go
@@ -1,5 +1,55 @@
 package csvconv
 
+import "fmt"
+
+type GatewayInterface[T any] interface {
+	ToCSV(v interface{}) (string, error)
+	FromCSV(v string) (interface{}, error)
+	SetInfo(mapping []*Mapping[T], record []string, objptr *T, fields map[string]interface{})
+	ColumnValue(field string) string
+}
+
+type Gateway[T any] struct {
+	Self    *T
+	Record  []string
+	Field   map[string]interface{}
+	Mapping []*Mapping[T]
+}
+
+func (x *Gateway[T]) SetInfo(mapping []*Mapping[T], record []string, objptr *T, fields map[string]interface{}) {
+	x.Mapping = mapping
+	x.Record = record
+	x.Self = objptr
+	x.Field = fields
+}
+
+func (Gateway[T]) ToCSV(v interface{}) (string, error) {
+	return fmt.Sprintf("%v", v), nil
+}
+
+func (Gateway[T]) FromCSV(v string) (interface{}, error) {
+	return v, nil
+}
+
+func (x *Gateway[T]) ColumnValue(column string) string {
+	for i, m := range x.Mapping {
+		if m.Column == column {
+			return x.Record[i]
+		}
+	}
+	return ""
+}
+
+type Mapping[T any] struct {
+	Column  string
+	Key     string
+	Gateway GatewayInterface[T]
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// ユーティリティ
+////////////////////////////////////////////////////////////////////////////////
+
 type EmptyString[T any] struct {
 	Gateway[T]
 }
